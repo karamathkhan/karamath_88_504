124b5aea3ea5330baf908be0dccf8736d57d8e9e
karamathkhan <31741078+karamathkhan@users.noreply.github.com>
2017-09-08 16:20:06 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student st=new Student(Sunny,520,19960804,66.5);
+		System.out.println("fullname is:"+ st.getFullName());
+		st.setFullName("raju");
+		st.setFullName("sanjay");
+		System.out.println("fullname is:"+ st.getFullName());
+		System.out.println("id is:"+ st.getId());
+		st.setId(560);
+		System.out.println("id is:"+ st.getId());
+		System.out.println("birthdate is:"+ st.getBirthDate());
+		st.setBirthDate(19950506);
+		System.out.println("birthdate is:"+ st.getBirthDate());
+		System.out.println("averagemarks is:"+ st.getAvgMark());
+		st.setAvgMark(65.66);
+		System.out.println("averagemarks is:"+ st.getAvgMark());
+		 compareTo(Student st){
+Student s1=(Student)o1;			 
+if(FullName==st.FullName)  
+		
+	return 0;
+else 
+ return -1;	
+		
+	}
+
+}
